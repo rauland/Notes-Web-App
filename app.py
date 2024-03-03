925e6af0468e9a761b77c3683a12ae0c29df4128
rauland <30706122+rauland@users.noreply.github.com>
2024-03-03 16:55:08 +1100

    removed request files and added DB connection

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-
-import find, insert, delete
 from flask import Flask, render_template, request, url_for, flash, redirect
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import cfg
+
+# Create a new client and connect to the server
+client = MongoClient(cfg.db_uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 app = Flask(__name__)
 
